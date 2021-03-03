58c50d98eb4d8b7bd97e2bb3450f28ccaaec0c99
PintTheDragon <PintTheDragon@hotmail.com>
2021-03-03 14:35:14 -0800

    Add ordinals

---
+++ scripts.js
@@ -0,0 +1,16 @@
+exports.ordinals = {en: ord_en};
+
+function ord_en(i){
+	const j = i % 10,
+        k = i % 100;
+    if (j === 1 && k !== 11) {
+        return i + "st";
+    }
+    if (j === 2 && k !== 12) {
+        return i + "nd";
+    }
+    if (j === 3 && k !== 13) {
+        return i + "rd";
+    }
+    return i + "th";
+}
